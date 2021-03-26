8947b870d3fbd4227c08689043c5ea3a56160241
shernk <31926953+shernk@users.noreply.github.com>
2021-03-27 00:29:46 +0700

    update npm

--- functions/handles/screams/screamsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const screams = express();
-
-const {getAllScreams} = require('./screamsControllers')
-
-screams.get("/screams", getAllScreams);
-
-exports.default = screams
-
